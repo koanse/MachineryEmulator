d421514a4830be859d37a14cbac43b218fa49ec8
koanse <ant.kondaurov@gmail.com>
2018-05-11 01:17:44 +0300

    Initial commit.

---
+++ Machinery/MainForm.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Machinery
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.tb3 = new System.Windows.Forms.TextBox();
+            this.wb = new System.Windows.Forms.WebBrowser();
+            this.nud = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nud)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(12, 25);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(123, 20);
+            this.tb1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Число деталей:";
+            // 
+            // tb2
+            // 
+            this.tb2.Location = new System.Drawing.Point(12, 51);
+            this.tb2.Name = "tb2";
+            this.tb2.Size = new System.Drawing.Size(123, 20);
+            this.tb2.TabIndex = 5;
+            // 
+            // tb3
+            // 
+            this.tb3.Location = new System.Drawing.Point(12, 77);
+            this.tb3.Name = "tb3";
+            this.tb3.Size = new System.Drawing.Size(123, 20);
+            this.tb3.TabIndex = 6;
+            // 
+            // wb
+            // 
+            this.wb.Location = new System.Drawing.Point(138, 9);
+            this.wb.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wb.Name = "wb";
+            this.wb.Size = new System.Drawing.Size(497, 341);
+            this.wb.TabIndex = 7;
+            // 
+            // nud
+            // 
+            this.nud.Location = new System.Drawing.Point(12, 161);
+            this.nud.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nud.Name = "nud";
+            this.nud.Size = new System.Drawing.Size(120, 20);
+            this.nud.TabIndex = 8;
+            this.nud.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Масштаб времени, мс:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 362);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nud);
+            this.Controls.Add(this.wb);
+            this.Controls.Add(this.tb3);
+            this.Controls.Add(this.tb2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Эмулятор технологического процесса машиностроительного производства";
+            ((System.ComponentModel.ISupportInitialize)(this.nud)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb2;
+        private System.Windows.Forms.TextBox tb3;
+        private System.Windows.Forms.WebBrowser wb;
+        private System.Windows.Forms.NumericUpDown nud;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Machinery/MainForm.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using MathNet.Numerics.Distributions;
+using System.IO;
+using System.Collections;
+
+namespace Machinery
+{
+    public partial class MainForm : Form
+    {
+        delegate void ReportDelegete(string s, MainForm form);
+        static List<int>[] arrQueue;
+        static int[][] arrUtil;
+        static int[][] arrTr = { new int[] { 0, 1, 3, 5 }, new int[] { 1, 2, 3, 5 }, new int[] { 0, 2, 3, 4, 5 } }; // 5 - склад
+        static Mutex mut = new Mutex();
+        static AutoResetEvent evDis = new AutoResetEvent(false);
+        static AutoResetEvent[][] evM;
+        static Queue<object[]> qDis;
+        public MainForm()
+        {
+            InitializeComponent();
+            tb1.Text = tb2.Text = tb3.Text = "10";
+        }
+        static void DisThreadProc(object o)
+        {
+            MainForm form = o as MainForm;
+            while (true)
+            {
+                evDis.WaitOne();
+                lock (qDis)
+                {
+                    while (qDis.Count > 0)
+                    {
+                        object[] arrO = qDis.Dequeue();
+                        int mType = (int)arrO[0], mIndex = (int)arrO[1];
+                        int pType = arrUtil[mType][mIndex];
+                        arrUtil[mType][mIndex] = -1;
+                        for (int i = 0; i < arrTr[pType].Length; i++)
+                        {
+                            if (arrTr[pType][i] == mType)
+                                arrQueue[arrTr[pType][i + 1]].Add(pType);
+                        }
+                    }
+                }
+                for (int i = 0; i < arrQueue.Length - 1; i++)
+                {
+                    if (arrQueue[i].Count == 0)
+                        continue;
+                    for (int j = 0; j < 3; j++)
+                    {
+                        while (arrQueue[i].Contains(j))
+                        {
+                            int k;
+                            for (k = 0; k < arrUtil[i].Length; k++)
+                            {
+                                if (arrUtil[i][k] == -1)
+                                {
+                                    arrQueue[i].Remove(j);
+                                    arrUtil[i][k] = j;
+                                    evM[i][k].Set();
+                                    break;
+                                }
+                            }
+                            if (k == arrUtil[i].Length)
+                                break;
+                        }
+                    }
+                }
+                string report = "Очереди:<br>";
+                for (int i = 0; i < 5; i++)
+                {
+                    report += string.Format("Тип станка: {0} - (", i);
+                    for (int j = 0; j < arrQueue[i].Count; j++)
+                    {
+                        report += string.Format(" {0} ", arrQueue[i][j]);
+                    }
+                    report += ")<br>";
+                }
+                report += "<br><br>Загрузка:<br>";
+                for (int i = 0; i < 5; i++)
+                {
+                    report += string.Format("Тип станка: {0} - (", i);
+                    for (int j = 0; j < arrUtil[i].Length; j++)
+                    {
+                        report += string.Format(" {0} ", arrUtil[i][j] == -1 ? "x" : arrUtil[i][j].ToString());
+                    }
+                    report += ")<br>";
+                }
+                report += string.Format("<br>Изготовлено: {0}", arrQueue[5].Count);
+                form.Invoke(new ReportDelegete(Report), report, form);
+            }
+        }
+        
+        static void MThreadProc(object obj)
+        {
+            object[] arrO = obj as object[];
+            int mType = (int)arrO[0], mIndex = (int)arrO[1], msec = (int)arrO[2];
+            while (true)
+            {
+                evM[mType][mIndex].WaitOne();
+                DateTime dtStart = DateTime.Now;
+                double time = -1;
+                if (mType == 0)
+                {
+                    NormalDistribution nd = new NormalDistribution(5, 2);
+                    time = nd.NextDouble();
+                }
+                else if (mType == 1)
+                    time = 3;
+                else if (mType == 2)
+                {
+                    NormalDistribution nd = new NormalDistribution(6, 1);
+                    time = nd.NextDouble();
+                }
+                else if (mType == 3)
+                {
+                    Random r = new Random();
+                    time = 2 + 4 * r.NextDouble();
+                }
+
+                else if (mType == 4)
+                {
+                    NormalDistribution nd = new NormalDistribution(7, 2);
+                    time = nd.NextDouble();
+                }
+                Thread.Sleep(Math.Abs((int)(time * msec)));
+                mut.WaitOne();
+                try
+                {
+                    StreamWriter sw = new StreamWriter("out.txt", true);
+                    sw.WriteLine(string.Format("Станок: {0}-{1}, начало: {2}, конец: {3}",
+                        mType, mIndex, dtStart, DateTime.Now));
+                    sw.Close();
+                }
+                finally
+                {
+                    mut.ReleaseMutex();
+                }
+                lock (qDis)
+                {
+                    qDis.Enqueue(arrO);
+                }
+                evDis.Set();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                arrQueue = new List<int>[6];
+                for (int i = 0; i < 6; i++)
+                {
+                    arrQueue[i] = new List<int>();
+                }
+                int[][] arr = { new int[] { -1, -1 }, new int[] { -1, -1, -1 }, new int[] { -1, -1 }, new int[] { -1, -1, -1 }, new int[] { -1 } };
+                arrUtil = arr;
+                evM = new AutoResetEvent[arr.Length][];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    evM[i] = new AutoResetEvent[arr[i].Length];
+                    for (int j = 0; j < arr[i].Length; j++)
+                    {
+                        evM[i][j] = new AutoResetEvent(false);
+                    }
+                }
+                qDis = new Queue<object[]>();
+                StreamWriter sw = new StreamWriter("out.txt", false);
+                sw.Write("");
+                sw.Close();
+                int msec = (int)nud.Value;
+                for (int i = 0; i < arrUtil.Length; i++)
+                {
+                    for (int j = 0; j < arrUtil[i].Length; j++)
+                    {
+                        Thread t = new Thread(new ParameterizedThreadStart(MThreadProc));
+                        t.IsBackground = true;
+                        t.Start(new object[] { i, j, msec });
+                    }
+                }
+                Thread td = new Thread(new ParameterizedThreadStart(DisThreadProc));
+                td.IsBackground = true;
+                td.Start(this);
+                for (int i = 0; i < int.Parse(tb1.Text); i++)
+                {
+                    arrQueue[0].Add(0);
+                }
+                for (int i = 0; i < int.Parse(tb2.Text); i++)
+                {
+                    arrQueue[1].Add(1);
+                }
+                for (int i = 0; i < int.Parse(tb3.Text); i++)
+                {
+                    arrQueue[0].Add(2);
+                }
+                button1.Enabled = false;
+                nud.Enabled = false;
+                evDis.Set();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        static void Report(string s, MainForm form)
+        {
+            form.wb.DocumentText = s;
+        }
+    }
+}
